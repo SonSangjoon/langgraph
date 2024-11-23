5c8c08963c32c7ce2462fe26e2075d93ecc91e2e
onshore <onshorecoding9@gmail.com>
2024-11-23 22:15:10 +0900

    add customer support bot

---
+++ customer_support_bot/agent.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnableConfig
+
+
+from tools.car_rental import (
+    book_car_rental,
+    cancel_car_rental,
+    search_car_rentals,
+    update_car_rental,
+)
+
+from tools.excursions import (
+    book_excursion,
+    cancel_excursion,
+    search_trip_recommendations,
+    update_excursion,
+)
+
+from tools.flight import (
+    cancel_ticket,
+    fetch_user_flight_information,
+    search_flights,
+    update_ticket_to_new_flight,
+)
+
+from tools.hotels import (
+    book_hotel,
+    cancel_hotel,
+    search_hotels,
+    update_hotel,
+)
+
+from tools.policies import lookup_policy
+from state import State
+
+
+load_dotenv()
+
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            configuration = config.get("configurable", {})
+            passenger_id = configuration.get("passenger_id", None)
+            state = {**state, "user_info": passenger_id}
+            result = self.runnable.invoke(state)
+            # If the LLM happens to return an empty response, we will re-prompt it
+            # for an actual response.
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+primary_assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Swiss Airlines. "
+            " Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
+
+part_1_tools = [
+    TavilySearchResults(max_results=1),
+    fetch_user_flight_information,
+    search_flights,
+    lookup_policy,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+]
+
+part_1_assistant_runnable = primary_assistant_prompt | llm.bind_tools(part_1_tools)
