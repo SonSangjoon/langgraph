b9cfddd99e5388915e6518b1924eeaf3e23b2972
onshore <onshorecoding9@gmail.com>
2024-11-23 20:18:27 +0900

    modify to use prebuild tool

--- quick_start/main.py
+++ quick_start/main.py
@@ -1,21 +1,19 @@
-import json
 from dotenv import load_dotenv
 
 from typing import Annotated
 from pydantic import BaseModel
 
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.messages import ToolMessage
 from langchain_openai import ChatOpenAI
 
-load_dotenv()
+from langgraph.graph import StateGraph, START
+from langgraph.graph.message import add_messages
 
-llm = ChatOpenAI(model="gpt-4o-mini")
-tool = TavilySearchResults(max_results=2)
-tools = [tool]
-llm_with_tools = llm.bind_tools(tools)
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
+
+memory = MemorySaver()
+load_dotenv()
 
 
 class State(BaseModel):
@@ -25,62 +23,18 @@ class State(BaseModel):
     messages: Annotated[list, add_messages]
 
 
-class BasicToolNode:
-    """Run tools requested in the last AIMessage node"""
+websearch = TavilySearchResults(max_results=2)
+tools = [websearch]
 
-    def __init__(self, tools: list) -> None:
-        # 도구 리스트
-        self.tools_list = {tool.name: tool for tool in tools}
-
-    def __call__(self, inputs: dict):
-        # 메시지가 존재할 경우 가장 최근 메시지 1개 추출
-        if messages := getattr(inputs, "messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("No message found in input")
-
-        # 도구 호출 결과
-        outputs = []
-        for tool_call in message.tool_calls:
-            # 도구 호출 후 결과 저장
-            tool_result = self.tools_list[tool_call["name"]].invoke(tool_call["args"])
-            outputs.append(
-                # 도구 호출 결과를 메시지로 저장
-                ToolMessage(
-                    content=json.dumps(
-                        tool_result, ensure_ascii=False
-                    ),  # 도구 호출 결과를 문자열로 변환
-                    name=tool_call["name"],
-                    tool_call_id=tool_call["id"],
-                )
-            )
-
-        return {"messages": outputs}
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm_with_tools = llm.bind_tools(tools)
 
 
 def chatbot(state: State):
     return {"messages": [llm_with_tools.invoke(state.messages)]}
 
 
-def route_tools(
-    state: State,
-):
-    """
-    Use in the conditional_edge to route to the ToolNode if the last message
-    has tool calls. Otherwise, route to the end.
-    """
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := getattr(state, "messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(f"No messages found in input state to tool_edge: {state}")
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
-    return END
-
-
-tool_node = BasicToolNode(tools=[tool])
+tool_node = ToolNode(tools=tools)
 
 graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
@@ -88,23 +42,30 @@ graph_builder.add_node("tools", tool_node)
 
 graph_builder.add_conditional_edges(
     "chatbot",
-    route_tools,
-    {"tools": "tools", END: END},
+    tools_condition,
 )
 
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("tools", "chatbot")
 
-graph = graph_builder.compile()
-
-
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+graph = graph_builder.compile(
+    checkpointer=memory,
+    interrupt_before=["tools"],
+)
 
 
 if __name__ == "__main__":
+    config = {"configurable": {"thread_id": "1"}}
+
+    def stream_graph_updates(user_input: str):
+        events = graph.stream(
+            {"messages": [("user", user_input)]},
+            config=config,
+            stream_mode="values",
+        )
+        for event in events:
+            event["messages"][-1].pretty_print()
+
     while True:
         user_input = input("User: ")
         if user_input.lower() in ["quit", "exit", "q"]:
